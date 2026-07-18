626bf2c0430e5f2c26af1d68e5981f39f81a7716
riadulislam111 <riadulislam.bd.pabna@gmail.com>
2026-07-18 16:01:37 +0600

    added all fils code udpats

--- break.js
+++ break.js
@@ -1,5 +1,20 @@
-for (let i = 0; i <= 10; i++) {
-    if(i > 5) {
-        break;
-    }
-}
+// for (let i = 0; i <= 10; i++) {
+//     if(i > 5) {
+//         break;
+//     }
+//     console.log(i)
+// }
+
+
+
+// let count = 0;
+// while (count < 10) {
+//     if(count > 4) {
+//         console.log("tor sathe breakup");
+//         break;
+//     }
+//     console.log('value of count', count);
+//     count++;
+// }
+// console.log('Life masti after breckup')
+
