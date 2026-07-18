8f8c93369d15e1f23f860f65879870ca3c138aa9
riadulislam111 <riadulislam.bd.pabna@gmail.com>
2026-07-18 18:23:41 +0600

    added all fils code lines

--- practic.js
+++ practic.js
@@ -49,3 +49,81 @@
 // console.log(sum)
 
 
+// tsk-7
+
+// let number = 7;
+
+// for(let i = 1; i <=10;i++) {
+//     console.log(number, "X" ,i ,"=", number * i);
+// }
+
+// let number = 7;
+// for(let i = 1; i <= 10;i++) {
+//     console.log(number, "X" ,i, "=", number * i)
+// }
+
+
+
+// task-8
+
+// for(let i = 20; i >=1; i--) {
+//     console.log(i)
+// }
+
+
+// task -9
+
+// for(let i = 2; i <= 50;i++) {
+//     if(i % 2 === 0) {
+//         console.log("even",i)
+//     }
+// }
+
+
+// tsk-10
+
+// for(let i = 1; i <=50;i++) {
+//     if(i > 30){
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// for(let i = 1; i <= 50; i++) {
+//     if(i > 30) {
+//         break;
+//     }
+//     console.log(i);
+// }
+
+
+
+// task-11
+
+// for(let i = 1; i<=20; i++) {
+//     if(i % 4 === 0){
+//         continue;
+//     }
+//     console.log(i)
+// }
+
+
+// task-12
+
+// let i = 1;
+
+// do{
+//     console.log(i);
+//     i++;
+// }while (i >= 5);
+
+
+
+// let i = 1;
+
+// do{
+//     console.log(i);
+//     i++;
+// }while (i >=5)
+
+
