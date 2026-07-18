800e9d9e027a271103470cd0e0e0dfc9a4ce4dd0
riadulislam111 <riadulislam.bd.pabna@gmail.com>
2026-07-18 16:57:35 +0600

    added all fils code line updats

---
+++ practic.js
@@ -0,0 +1,51 @@
+// task 1 
+
+// for(let i = 1; i <=5; i++) {
+//     console.log("Hello")
+// }
+
+
+// tsk-2
+
+// for(let i = 1; i <=10; i++){
+//     console.log(i)
+// }
+
+// tsk-3
+
+// let count = 10;
+
+// while(count >0) {
+//     console.log(count);
+//     count--;
+// }
+
+// task-4
+
+// let i = 1;
+// while(i <=7) {
+//     console.log("Loop cholse");
+//     i++;
+// }
+
+
+// task-5
+
+// let i = 1;
+// while(i <= 30){
+//     if(i % 2 === 0){
+//         console.log(i)
+//     }
+//     i++;
+// }
+
+
+// tsk-6
+// let sum = 0;
+// for(let i = 1; i <= 20; i++) {
+//     sum = sum + i;
+    
+// }
+// console.log(sum)
+
+
